459d3049f2e0f688d4ddf050291bf9fe7f73ed61
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-04-12 22:54:14 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const miniCss = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 let mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+// let mode = 'production'
 
 module.exports = {
   mode: mode,
@@ -22,18 +24,33 @@ module.exports = {
     new htmlWebpackPlugin({
       template: 'index.html'
     }),
+    new CopyPlugin({
+      patterns: [
+        {from: '/img', to: './img/'},
+        {from: './node_modules/three/examples/jsm/loaders/DRACOLoader.js', to: './'}
+      ]
+    }),
     new CleanWebpackPlugin()
   ],
-
   module: {
-    rules: [{
-      test: /\.(s*)css$/,
-      use: [
-        miniCss.loader,
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|jpg|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          miniCss.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+
+    ]
   }
 
 };
